a5632fbbecd33edca5ed73c194fc6d5de2fa100d
karan.shgl96@gmail.com <karan.shgl96@gmail.com>
2017-06-24 04:14:47 +0530

    Added Sidebar.js + Dynamic Sidebar.vue

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,41 +2,43 @@
   <div class="flow">
     <div class="sec-item">
       <h2 class="title"><i class="fa fa-bar-chart"></i>Skills</h2>
-        <h3 class="skill-item">Programming</h3>
-        <div class="row skills">
+        <h3 class="skill-item">{{data.code.title}}</h3>
+        <div class="meta">
+        <div class="row skills" v-for="(item,i) in data.code.list">
           <div class="col-md-4 s-li">
-            Python
+            {{item.name}}
           </div>
           <div class="col-md-8 s-bar">
-            <ui-progress-linear type="determinate" :progress="proPercent"></ui-progress-linear>
+            <ui-progress-linear type="determinate" :progress="item.percent"></ui-progress-linear>
           </div>
         </div>
+
+        <h3 class="skill-item">{{data.lang.name}}</h3>
+        <div class="col-md-6 lang" v-for="la in data.lang.list">
+          {{la}}
+        </div>
+        <div class="clearfix">
+
+        </div>
+
+          <h2 class="title"><i class="fa fa-edit"></i>{{data.hobby.name}}</h2>
+          <div class="col-md-6 lang" v-for="i in data.hobby.list">
+            {{i}}
+          </div>
+        </div>
+
 </div>
 </div>
 </template>
 
 
 <script>
+import dataSidebar from '../sidebar.json'
 export default{
   data(){
     return{
-      proPercent : 0,
-      data : 40
+      data : dataSidebar
     }
-  },
-  created: function(){
-    var self=this;
-    self.proPercent=0;
-    setTimeout(()=>{
-      var p = setInterval(function () {
-        self.proPercent+=1;
-        if(self.proPercent>=self.data){
-          clearInterval(p);
-        }
-      }, 20);
-    },750)
-
-
   }
 }
 
@@ -47,19 +49,25 @@ export default{
 .skill-item{
   color: #3F4650;
   font-size: 16px;
-  margin-top: 0;
-  margin-bottom: 0;
+  margin-top: 20px;
+  margin-bottom: 15px;
   padding-left: 5px;
   font-weight: 700;
   border-left: solid #2d7788;
 }
 
 .skills{
-  margin-top: 10px;
+  padding: 3px 10px;
+  margin-bottom: 10px;
 }
 
-.s-li{
+.lang{
+  padding: 3px 10px;
+  margin-bottom: 20px;
+}
 
+.meta{
+  margin-bottom: 30px;
 }
 
 .s-bar{
